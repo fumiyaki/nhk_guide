c3a9f0eefcb2b7e95cd53a016735eb88a74bfcb7
fumiya yono <z2ab8q3ty0@gmail.com>
2021-01-19 23:11:15 +0900

    [add]mockデータの追加と型の追加
    APIから取得できるデータを例にGuideMockDataの追加
    型の追加

---
+++ src/types/guide.ts
@@ -0,0 +1,34 @@
+export type Guide = {
+  id: string;
+  event_id: string;
+  start_time: string;
+  end_time: string;
+  area: {
+    id: string;
+    name: string;
+  };
+  service: {
+    id: string;
+    name: string;
+    logo_s: {
+      url: string;
+      width: string;
+      height: string;
+    };
+    logo_m: {
+      url: string;
+      width: string;
+      height: string;
+    };
+    logo_l: {
+      url: string;
+      width: string;
+      height: string;
+    };
+  };
+  title: string;
+  subtitle: string;
+  content: string;
+  act: string;
+  genres: string[];
+};
