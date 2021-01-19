143bef1a5d4ba6b3d58b6d77751dba6d2de3a754
fumiya yono <z2ab8q3ty0@gmail.com>
2021-01-20 01:45:15 +0900

    [update]データをAPI経由で取得するように修正
    GuideListに表示させるデータをAPI経由で取得するように修正

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
-import { mockGuideListData } from "./src/utils/mockData";
 import { GuideList } from "./src/components/organisms/GuideList";
+import axios from "axios";
+import { NHK_API_KEY, NHK_API_URL } from "@env";
+
+const area = "130";
+const service = "g1";
+const date = "2021-01-20";
+const url = `${NHK_API_URL}/list/${area}/${service}/${date}.json?key=${NHK_API_KEY}`;
 
 export default function App() {
+  const [guideList, setGuideList] = useState([]);
+  useEffect(() => {
+    getNHKGuideData();
+  }, []);
+
+  const getNHKGuideData = async () => {
+    const response = await axios.get(url);
+    setGuideList(response.data.list.g1);
+  };
+
   return (
     <SafeAreaView>
-      <GuideList guideList={mockGuideListData} />
+      <GuideList guideList={guideList} />
     </SafeAreaView>
   );
 }
