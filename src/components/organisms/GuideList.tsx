7c262f092dc32d5f4eff867a4766b1062ca64164
fumiya yono <z2ab8q3ty0@gmail.com>
2021-01-20 17:09:44 +0900

    [update]undefinedを受け取ったらNo Dataと表示させる
    undefinedを受け取っても別処理に移行できる準備をする

--- src/components/organisms/GuideList.tsx
+++ src/components/organisms/GuideList.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, View, Text } from "react-native";
 import { GuideCard } from "../../components/molecules/GuideCard";
 import { Guide } from "../../../src/types/guide";
 
 type Props = {
-  guideList: Guide[];
+  guideList: Guide[] | undefined;
 };
 
 export const GuideList: React.FC<Props> = ({ guideList }: Props) => {
   return (
-    <FlatList
-      data={guideList}
-      renderItem={({ item }: { item: Guide }) => <GuideCard guide={item} />}
-      keyExtractor={(item, index) => index.toString()}
-    />
+    <View>
+      {guideList === undefined ? (
+        <Text>No Data</Text>
+      ) : (
+        <FlatList
+          data={guideList}
+          renderItem={({ item }: { item: Guide }) => <GuideCard guide={item} />}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      )}
+    </View>
   );
 };
