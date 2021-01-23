34864d73b99df3928e5f23e9c18e0fc01b9c9878
fumiya yono <z2ab8q3ty0@gmail.com>
2021-01-24 06:16:47 +0900

    [fix]型の定義の修正
    ・｜でmerge出来ると勘違いしていた。

--- src/types/guide.ts
+++ src/types/guide.ts
@@ -33,14 +33,44 @@ export type Guide = {
   genres: string[];
 };
 
-export type GuideDetail =
-  | Guide
-  | {
-      program_logo: {
-        url: string;
-        width: string;
-        height: string;
-      };
-      program_url: string;
-      hashtags: string[];
+export type GuideDetails = {
+  id: string;
+  event_id: string;
+  start_time: string;
+  end_time: string;
+  area: {
+    id: string;
+    name: string;
+  };
+  service: {
+    id: string;
+    name: string;
+    logo_s: {
+      url: string;
+      width: string;
+      height: string;
     };
+    logo_m: {
+      url: string;
+      width: string;
+      height: string;
+    };
+    logo_l: {
+      url: string;
+      width: string;
+      height: string;
+    };
+  };
+  title: string;
+  subtitle: string;
+  content: string;
+  act: string;
+  genres: string[];
+  program_logo: {
+    url: string;
+    width: string;
+    height: string;
+  };
+  program_url: string;
+  hashtags: string[];
+};
